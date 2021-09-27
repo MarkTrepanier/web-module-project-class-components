d5ebb011dda6f4a3e2b04a5dd1ca7db6182c5967
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-27 16:51:03 -0400

    basic structure. no functionality

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <div className='App'>
         <h2>Welcome to your Todo App!</h2>
+        <div className='todoList'>
+          <h3 className='todo'>list item</h3>
+          <h3 className='todo'>list item</h3>
+          <h3 className='todo'>list item</h3>
+        </div>
+        <div className='todoForm'>
+          <button>add todo</button>
+          <button>clear completed</button>
+        </div>
       </div>
     );
   }
