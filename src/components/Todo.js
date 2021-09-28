f0ef09e084489260d40170dcf8e6a28a6b7a9a84
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-27 21:37:17 -0400

    console.log removed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,10 +3,7 @@ import React from "react";
 const Todo = props =>{
     
     const handleClick=()=>{
-        console.log(props.id)
-        console.log(props.completed)
         props.handleToggle(props.id)
-        console.log(props.completed)
     }
     return(
     <li id={props.id} onClick={handleClick}>{props.task}</li>
